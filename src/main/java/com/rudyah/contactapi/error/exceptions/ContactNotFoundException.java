13b6f81c6fbd24fce30dd65a0d3de42902c08394
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-02-20 19:43:33 -0800

    ContactNotFoundException add response status

--- src/main/java/com/rudyah/contactapi/error/exceptions/ContactNotFoundException.java
+++ src/main/java/com/rudyah/contactapi/error/exceptions/ContactNotFoundException.java
@@ -1,5 +1,9 @@
 package com.rudyah.contactapi.error.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ContactNotFoundException extends RuntimeException{
     public ContactNotFoundException(String errorMessage) {
         super(errorMessage);
